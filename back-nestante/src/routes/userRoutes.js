ba8a383cbe0f85099da1ad6be1aedaf0a9e5717a
Marcus Phellypp <marcus_psc@hotmail.com>
2024-12-21 15:19:05 -0300

    ✨ Adiciona rotas de usuário para rrecuperação e redefinição de senha

--- back-nestante/src/routes/userRoutes.js
+++ back-nestante/src/routes/userRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { createUser, getUsers, loginUser } = require("../controllers/userController");
+const { createUser, getUsers, loginUser, resetPassword, requestPasswordReset } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/register", createUser); // Rota para criar usuário
 router.post("/login", loginUser); // Rota para login de usuário
 router.get("/", getUsers); // Rota para listar usuários (testes)
+
+//recuperação de senha
+router.post("/request-password-reset", requestPasswordReset);
+router.post("/reset-password", resetPassword);
+
 
 module.exports = router;
