89e9a85b3e0c84aa13698e1c5a9d66675756d1bf
Marcus Phellypp <marcus_psc@hotmail.com>
2024-12-21 21:42:47 -0300

    🔧 Atualizar main.js para incluir PrimeVue e ComponentsPrimevue

--- app-nestante/src/main.js
+++ app-nestante/src/main.js
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import 'primeicons/primeicons.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +7,22 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura'
+import ComponentsPrimevue from './components/ComponentsPrimevue'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.dark-mode',
+        }
+    }
+});
+app.use(ComponentsPrimevue)
 
 app.mount('#app')
